0e59599938e7822bf09c883e9392bea74eacc8c0
gustavoaamorim <amorim.gustavo@outlook.com>
2022-10-17 22:36:34 -0300

    update tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* existem 8 tipos primitivos:
+        int, byte, short, long, float, double, boolean, char.
+        Os tipos mais usuais são int e double */
+        
+        //declarando uma variável
+
+        int numero = 10;
+        
+        //declaração de constante
+
+        final double PI = 3.14;
+        
     }
 }
