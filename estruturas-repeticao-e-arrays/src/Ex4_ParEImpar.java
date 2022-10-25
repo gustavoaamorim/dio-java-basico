ea4bf106cdd1f487bacc8f411fe5914510b19401
gustavoaamorim <amorim.gustavo@outlook.com>
2022-10-24 21:56:05 -0300

    add exercício 3

---
+++ estruturas-repeticao-e-arrays/src/Ex4_ParEImpar.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex4_ParEImpar {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+    }
+    
+}
